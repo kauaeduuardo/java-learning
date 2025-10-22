99466f48fc272f4e62daf1c91a8038a7f7030295
Kauã Eduardo <154907062+kauaeduuardo@users.noreply.github.com>
2025-10-22 15:28:37 -0300

    Update

---
+++ src/fundamentos/tiposdedados/TiposPrimitivos.java
@@ -0,0 +1,19 @@
+package fundamentos.tiposdedados;
+
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        // Todos os tipos primitivos são numéricos
+        // int, double, float, char, short, long, byte, boolean
+
+        int numeroInteiro = 5; // long, short e byte também são inteiros
+        double numeroRealPrecisaoDupla = 5.67;
+        float numeroReal = 5.5f;
+
+        char caractere = 'K';
+
+        boolean verdadeiro = true;
+        boolean falso = false;
+
+        // Obs: Em Java, string não é um tipo primitivo, é uma classe
+    }
+}
---
+++ src/introducao/SintaxeBasica.java
@@ -0,0 +1,18 @@
+package introducao;
+
+import java.util.Scanner;
+
+public class SintaxeBasica {
+    public static void main(String[] args) {
+        // Criar variável
+        int numero = 5;
+        String nome = "Kauã";
+
+        // comando de saída
+        System.out.println("Saída.");
+
+        // comando de entrada
+        Scanner scan = new Scanner(System.in);
+        int numero2 = scan.nextInt();
+    }
+}
